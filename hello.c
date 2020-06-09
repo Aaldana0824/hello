44f2e4e05bb58a8f8222858bf69e0753148fae66
Aaldana0824 <59975303+Aaldana0824@users.noreply.github.com>
2020-06-09 09:16:04 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Austin Aldana
+ * Date: 2020/06/09
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+  int main(int argc, char **argv) {
+
+    printf("Austin Aldana\n Christian White\n");
+
+    return 0;
+  }
